ea6397688c91bf7d48ff4dec7aff9fa21c348592
Prithviraj Yuvaraj <prith@Prithvirajs-MacBook-Pro.local>
2023-06-21 12:37:29 -0700

    Corrected bundle

--- src/krnl.cpp
+++ src/krnl.cpp
@@ -4,7 +4,7 @@ extern "C" {
     void krnl(data_t* a, data_t* b, data_t* c) {
         #pragma HLS INTERFACE m_axi port = a bundle = gmem0
         #pragma HLS INTERFACE m_axi port = b bundle = gmem1
-        #pragma HLS INTERFACE m_axi port = c bundle = gmem0
+        #pragma HLS INTERFACE m_axi port = c bundle = gmem2
 
         for (int i = 0; i < DATA_SIZE; i++) {
             #pragma HLS UNROLL //factor=2
